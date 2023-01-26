9226c0d5470cafcfe5b7a6763c74891057f77e79
Jake Overall <joverall22@gmail.com>
2023-01-26 14:31:23 -0700

    adds modal for reports

--- helpReviews.client/src/components/Modal.vue
+++ helpReviews.client/src/components/Modal.vue
@@ -3,13 +3,13 @@
     <div class="modal-dialog">
       <div class="modal-content">
         <div class="modal-header">
-          <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
-          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+          <slot name="modal-header">
+            <h1 class="modal-title fs-5" id="exampleModalLabel">{{ modalTitle }}</h1>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+          </slot>
         </div>
 
-        <slot>
-
-        </slot>
+        <slot></slot>
 
       </div>
     </div>
@@ -19,6 +19,13 @@
 
 <script>
 export default {
+  props: {
+    modalTitle: {
+      type: String,
+      required: false,
+      default: 'Close'
+    },
+  },
   setup() {
     return {}
   }
